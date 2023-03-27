34b9cfbd36ab659fe2455a540c77a9ce6f0bcefb
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2023-03-26 20:45:19 -0700

    currency exchange

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import discord
+from discord.ui import Button, View
 from utils import *
 
 load_dotenv()  # load variables from .env file
@@ -20,14 +21,32 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith("#price"):
+    if message.content.startswith('#help'):
+        await message.channel.send(f"Commands:\n**#price** *<ticker>*\n**#curex** *<USD/JPY>* *<1>*", reference=message)
+
+    elif message.content.startswith("#price"):
         ticker = message.content.split(' ')
         if len(ticker) > 1:
             price = get_price(ticker[1])
+            print(price)
             if "price" in price:
                 price = price["price"]
-                await message.channel.send(f"{ticker[1]}: ${price}", reference=message)
+                await message.channel.send(f"{ticker[1]}: **${price}**", reference=message)
             else:
                 await message.channel.send("Enter a valid symbol", reference=message) 
+
+    elif message.content.startswith("#curex"):
+        strs = message.content.split(' ')
+        if len(strs) == 3:
+            currency = strs[1]
+            amount = strs[2]
+
+            rate = currency_conversion(currency, amount)
+            if 'rate' in rate:
+                data = currency.split('/')
+                await message.channel.send(f"Conversion rate is: **{rate['rate']}** \n **1** {data[0]} = **{rate['amount']}** {data[1]}", reference=message)
+            else:
+                await message.channel.send("Invalid params", reference=message)
+
 
 client.run(BOT_TOKEN)
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import discord
 import requests
 import os
 
+
 def get_price(ticker):
     API_KEY = os.getenv('API_KEY')
     url = "https://twelve-data1.p.rapidapi.com/price"
@@ -15,4 +16,18 @@ def get_price(ticker):
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     return response.json()
+
+
+def currency_conversion(c, amount):
+    API_KEY = os.getenv('API_KEY')
+    url = "https://twelve-data1.p.rapidapi.com/currency_conversion"
+    querystring = {"symbol": c,"amount":amount}
+    headers = {
+    	"X-RapidAPI-Key": API_KEY,
+    	"X-RapidAPI-Host": "twelve-data1.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.json()
+
     
